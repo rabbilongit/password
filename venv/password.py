d0f8b4d8d64ecb6e6e1259607c1a9e9d22d2b2d1
rabb1l <mohd.rabbil100@gmail.com>
2021-10-09 19:19:09 +0530

    Makes a password by entering inputs

---
+++ venv/password.py
@@ -0,0 +1,16 @@
+import random
+
+print("***First letter of every word in CAPS***")
+fav_car = str(input("FAV. CAR:"))
+fav_city =str(input("FAV. CITY:"))
+mother_name =str(input("MOTHER'S NAME:"))
+father_name = str(input("FATHER'S NAME:"))
+fav_rapper = str(input("FAV. RAPPER:"))
+
+numbers = random.randint(0,99)
+spc_character = random.choice("_@#$%&*-")
+names = [fav_car,fav_rapper,fav_city,mother_name,father_name , numbers ,spc_character]
+name1 = random.choice(names)
+name2 = random.choice(names)
+if name1 != name2:
+    print(f"{name2}{spc_character}{name1}{numbers}")
